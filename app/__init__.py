8b0717fed8530768744429bf1dd754e949305f8f
Brittany McGarr <brittanymcgarr@nevada.unr.edu>
2017-06-07 05:24:46 +0000

    S3 Bucket awaaaaaaay

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,18 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_googlemaps import GoogleMaps
+from flask_s3 import FlaskS3
+
 from config import base_directory, GOOGLEMAPS_KEY
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+s3 = FlaskS3()
+s3.init_app(app)
+
 db = SQLAlchemy(app)
+
 GoogleMaps(app, key=GOOGLEMAPS_KEY)
 
 lm = LoginManager()
--- config.py
+++ config.py
@@ -20,3 +20,4 @@ IMAGES_DIR = os.path.join(STATIC_DIR, 'images')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 GOOGLEMAPS_KEY = os.environ.get('GOOG_KEY')
+FLASKS3_BUCKET_NAME = 'orange-collar'
